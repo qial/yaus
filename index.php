c3ccd11edb237a25818c8bb2ad5febc4a185981d
Kyle Watson <kw@headwire.com>
2012-07-03 07:19:18 -0700

    Added first files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<html>
+<head>
+<title>Yaus, yet another URL shortener</title>
+</head>
+<body>
+<div id="urlbox">
+	<div class="url-input">
+		<form method="post" action="/n/">
+		<input type="text" name="url" />
+		<br/>
+		<input type="submit"/>
+		</form>
+	</div>
+</div>
+</div>
+</body>
+</html>
