4603b99501d69899c0ef9f96eda20880a4c7dadd
Kyle Watson <kw@headwire.com>
2012-07-03 15:09:59 -0700

    Modified yaus functions, added BASE_URL property

--- yaus.php
+++ yaus.php
@@ -1,7 +1,28 @@
 <?php
 
+require_once 'yaus_vars.php';
+
 function id2code($id) {
 	return $id;
+}
+
+function yaus_connect() {
+	$conn = mysql_connect($DB_HOST, $DB_USER, $DB_PASSWORD);
+	if(!$conn) {
+		die('Could not connect: '.mysql_error());
+	}
+	
+	mysql_select_db("yaus",$conn);
+	
+	return $conn;
+}
+
+function get_url($str) {
+	return $BASE_URL . $str;
+}
+
+function get_loc($str) {
+	return "Location: ".get_url($str);
 }
 
 ?>
--- yaus_vars_blank.php
+++ yaus_vars_blank.php
@@ -1,7 +1,9 @@
 <?php
 
-$DB_HOST = "localhost";
-$DB_USER = "root";
-$DB_PASSWORD = "root";
+$DB_HOST = "";
+$DB_USER = "";
+$DB_PASSWORD = "";
+
+$BASE_URL = "";
 
 ?>
